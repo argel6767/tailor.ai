4d452d583d3d5e3010ef75d5b489a227fd503374
Argel Hernandez <argel6767@gmail.com>
2024-10-30 10:57:21 -0400

    add ChatSession

--- tailor.ai/src/main/java/com/argel6767/tailor/ai/chat_session/ChatSession.java
+++ tailor.ai/src/main/java/com/argel6767/tailor/ai/chat_session/ChatSession.java
@@ -1,15 +1,31 @@
 package com.argel6767.tailor.ai.chat_session;
 
+import com.argel6767.tailor.ai.user.User;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import jdk.jfr.Timestamp;
+
+import java.time.LocalDateTime;
 
 @Entity
 public class ChatSession {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(nullable = false, updatable = false)
     private Long chatSessionId;
 
+    @Column(nullable = false, columnDefinition = "varchar(100) default 'New Chat'")
+    private String chatSessionName;
+    
+    @Timestamp
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+    
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+    
     public void setChatSessionId(Long chatSessionId) {
         this.chatSessionId = chatSessionId;
     }
@@ -17,4 +33,29 @@ public class ChatSession {
     public Long getChatSessionId() {
         return chatSessionId;
     }
+
+    public String getChatSessionName() {
+        return chatSessionName;
+    }
+
+    public void setChatSessionName(String chatSessionName) {
+        this.chatSessionName = chatSessionName;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
 }
