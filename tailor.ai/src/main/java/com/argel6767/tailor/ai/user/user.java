f2c2e31567c2642a746f3ff5dee24051cb8c0dbd
Argel Hernandez <argel6767@gmail.com>
2024-10-30 10:30:30 -0400

    add User entity

---
+++ tailor.ai/src/main/java/com/argel6767/tailor/ai/user/user.java
@@ -0,0 +1,88 @@
+package com.argel6767.tailor.ai.user;
+
+import jakarta.persistence.*;
+import jdk.jfr.Timestamp;
+
+import java.time.LocalDateTime;
+
+import java.util.List;
+
+@Table(name = "users")
+@Entity
+public class user{
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(nullable = false, updatable = false)
+    private Long userId;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private Boolean isEmailVerified;
+
+    @Column(nullable = false)
+    private String passwordHash;
+
+    @Column(nullable = false)
+    private String profession;
+
+    @Column(nullable = false)
+    @Timestamp
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false)
+    @Timestamp
+    private LocalDateTime lastLogin;
+
+    @OneToMany(mappedBy="user")
+    private List<ChatSession> chatSessions;
+
+    public void setId(Long id) {
+        this.userId = id;
+    }
+
+    public Long getId() {
+        return userId;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getProfession() {
+        return profession;
+    }
+
+    public void setProfession(String profession) {
+        this.profession = profession;
+    }
+
+    public Boolean getIsEmailVerified() {
+        return isEmailVerified;
+    }
+
+    public void setIsEmailVerified(Boolean isEmailVerified) {
+        this.isEmailVerified = isEmailVerified;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getLastLogin() {
+        return lastLogin;
+    }
+
+    public void setLastLogin(LocalDateTime lastLogin) {
+        this.lastLogin = lastLogin;
+    }
+}
