185438afefc7b201be934d7c837efe7020c5cdd7
Argel Hernandez <argel6767@gmail.com>
2024-10-29 21:54:51 -0400

    batman

---
+++ tailor.ai/src/test/java/com/argel6767/tailor/ai/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.argel6767.tailor.ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
