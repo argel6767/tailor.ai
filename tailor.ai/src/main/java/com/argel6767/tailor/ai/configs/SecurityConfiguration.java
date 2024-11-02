80c4ab93eb3266b9c8acde226f632ee5da105dc6
Argel Hernandez <argel6767@gmail.com>
2024-11-02 00:49:53 -0400

    add SecurityConfiguration

---
+++ tailor.ai/src/main/java/com/argel6767/tailor/ai/configs/SecurityConfiguration.java
@@ -0,0 +1,73 @@
+package com.argel6767.tailor.ai.configs;
+
+import com.argel6767.tailor.ai.jwt.JwtAuthenticationFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
+
+/**
+ * Configures custom security rules for end points
+ * ie what endpoints are open to everyone and which need JWT Token and set session management to
+ * stateless
+ * Also configures CORS for to allow for development on localhost
+ */
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration{
+
+    private final AuthenticationProvider authenticationProvider;
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    public SecurityConfiguration(AuthenticationProvider authenticationProvider, JwtAuthenticationFilter jwtAuthenticationFilter) {
+        this.authenticationProvider = authenticationProvider;
+        this.jwtAuthenticationFilter = jwtAuthenticationFilter;
+    }
+
+    /*
+     * configures the security rules of endpoints
+     */
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity ) throws Exception {
+        httpSecurity.csrf(csrf -> {
+                            try {
+                                csrf.disable()
+                                        .authorizeHttpRequests(authorize -> authorize
+                                                .requestMatchers("/auth/*").permitAll()
+                                                .anyRequest().authenticated());
+                            } catch (Exception e) {
+                                throw new RuntimeException(e);
+                            }
+                        }
+                )
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .authenticationProvider(authenticationProvider)
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+        return httpSecurity.build();
+    }
+
+    /*
+     * configures allowing CORS for any local host port for testing
+     */
+    @Bean
+    CorsConfiguration corsConfiguration() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedOrigin("http://localhost:*");
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedMethod("*");
+
+        UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+        urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
+        return corsConfiguration;
+    }
+
+}
