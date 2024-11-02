9fa559cf82ecc3e8a28384c4822fedd01211e117
Argel Hernandez <argel6767@gmail.com>
2024-11-02 11:48:38 -0400

    add EmailService

---
+++ tailor.ai/src/test/java/com/argel6767/tailor/ai/email/EmailService.java
@@ -0,0 +1,32 @@
+package com.argel6767.tailor.ai.email;
+
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+/**
+ * houses the business logic of formatting an email
+ */
+@Service
+public class EmailService {
+
+    @Autowired
+    private JavaMailSender mailSender;
+
+    /*
+     * sending verification email
+     * takes in to whom its being sent to & subject and body
+     */
+    public void sendEmail(String to, String subject, String body) throws MessagingException {
+        MimeMessage message = mailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message, true);
+        helper.setTo(to);
+        helper.setSubject(subject);
+        helper.setText(body, true);
+        mailSender.send(message);
+    }
+}
