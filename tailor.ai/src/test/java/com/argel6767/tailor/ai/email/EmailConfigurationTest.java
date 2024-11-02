5b79302ec1c96cc247c50dad1926fee383eba926
Argel Hernandez <argel6767@gmail.com>
2024-11-02 11:39:01 -0400

    add test for EmailConfiguration

--- tailor.ai/src/test/java/com/argel6767/tailor/ai/email/EmailConfigurationTest.java
+++ tailor.ai/src/test/java/com/argel6767/tailor/ai/email/EmailConfigurationTest.java
@@ -1,14 +1,17 @@
 package com.argel6767.tailor.ai.email;
 
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.MockitoAnnotations;
-import org.springframework.context.annotation.Bean;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 
+import java.util.Properties;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 class EmailConfigurationTest {
 
@@ -23,8 +26,13 @@ class EmailConfigurationTest {
 
     }
     @Test
-    void javaMailSender() {
-
-        emailConfiguration.javaMailSender();
+    void testJavaMailSender() {
+        JavaMailSenderImpl javaMailSender = mock(JavaMailSenderImpl.class);
+        when(javaMailSender.getUsername()).thenReturn(EMAIL_ADDRESS);
+        when(javaMailSender.getPassword()).thenReturn(EMAIL_PASSWORD);
+        JavaMailSender mailSender = emailConfiguration.javaMailSender();
+        assertNotNull(mailSender);
+        assertEquals(EMAIL_ADDRESS, javaMailSender.getUsername());
+        assertEquals(EMAIL_PASSWORD, javaMailSender.getPassword());
     }
 }
