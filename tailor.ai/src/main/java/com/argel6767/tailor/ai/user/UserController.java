a71b6b28926323336ffc93ae98839bd30dcf9df7
Argel Hernandez <argel6767@gmail.com>
2024-11-03 00:12:31 -0400

    add UserController

---
+++ tailor.ai/src/main/java/com/argel6767/tailor/ai/user/UserController.java
@@ -0,0 +1,24 @@
+package com.argel6767.tailor.ai.user;
+
+import com.argel6767.tailor.ai.user.requests.AddProfessionRequest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/user")
+@RestController
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PutMapping("/profession")
+    public ResponseEntity<User> updateProfession(AddProfessionRequest addProfessionRequest) {
+        User user = userService.addProfession(addProfessionRequest.getEmail(), addProfessionRequest.getProfession());
+        return ResponseEntity.ok(user);
+    }
+}
