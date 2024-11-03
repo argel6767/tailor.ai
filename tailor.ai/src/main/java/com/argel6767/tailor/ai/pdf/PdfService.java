343a1eddd61d7db0adc787b7cb713604ab3fceb9
Argel Hernandez <argel6767@gmail.com>
2024-11-03 01:38:54 -0500

    add PdfService

---
+++ tailor.ai/src/main/java/com/argel6767/tailor/ai/pdf/PdfService.java
@@ -0,0 +1,24 @@
+package com.argel6767.tailor.ai.pdf;
+
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.text.PDFTextStripper;
+import org.springframework.stereotype.Service;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * holds the service logic for reading pdf files to allow for chat completions to read the file
+ */
+@Service
+public class PdfService {
+
+    public String readFile(File file) throws IOException {
+        PDDocument document = PDDocument.load(file);
+        PDFTextStripper stripper = new PDFTextStripper();
+        stripper.setSortByPosition(true);
+        String text = stripper.getText(document);
+        document.close();
+        return text;
+    }
+}
---
+++ tailor.ai/src/test/java/com/argel6767/tailor/ai/pdf/PdfServiceTest.java
@@ -0,0 +1,23 @@
+package com.argel6767.tailor.ai.pdf;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+
+import java.io.File;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PdfServiceTest {
+
+    @Mock
+    File file;
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void readFile() {
+    }
+}
