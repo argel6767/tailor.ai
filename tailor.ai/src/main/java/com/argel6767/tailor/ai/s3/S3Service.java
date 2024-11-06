a5582bbbdadb70bb8bd5f92bcad38fe72d3a9677
argel6767 <argel6767@gmail.com>
2024-11-05 22:18:08 -0500

    adds S3Service

--- tailor.ai/src/main/java/com/argel6767/tailor/ai/s3/S3Service.java
+++ tailor.ai/src/main/java/com/argel6767/tailor/ai/s3/S3Service.java
@@ -2,17 +2,26 @@ package com.argel6767.tailor.ai.s3;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.io.InputStreamSource;
+import org.springframework.core.io.InputStreamResource;
+
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import software.amazon.awssdk.core.Response;
 import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.GetObjectRequest;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
 import java.io.File;
-import java.nio.ByteBuffer;
 
+import java.io.InputStream;
+
+
+/**
+ * handles the service logic for uploading and downloading files from the s3 bucket
+ */
 @Service
 public class S3Service {
 
@@ -26,12 +35,42 @@ public class S3Service {
         return chatSessionId + "-resume";
     }
 
+    /*
+    * uploads given file to the s3 bucket
+     */
     public void uploadFile(String key, File userPdf) {
          s3client.putObject(PutObjectRequest.builder().bucket(bucket).key(key).build(), RequestBody.fromFile(userPdf));
     }
 
-    public ResponseEntity<InputStreamSource> downloadFile(String key) {
-         
+    /*
+     * grabs pdf file as a stream to allow for rendering on the fronted.
+     */
+    public ResponseEntity<InputStreamResource> downloadFile(String key) {
+        GetObjectRequest getObjectRequest = GetObjectRequest.builder().bucket(bucket).key(key).build();
+        InputStream inputStream = s3client.getObject(getObjectRequest);
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_PDF);
+        headers.add(HttpHeaders.CONTENT_DISPOSITION, "inline; filename=\"" + key + ".pdf\"");
+        return ResponseEntity.ok().headers(headers).body(new InputStreamResource(inputStream);
+    }
+
+    /*
+     * getters and setters
+     */
+    public S3Client getS3client() {
+        return s3client;
+    }
+
+    public void setS3client(S3Client s3client) {
+        this.s3client = s3client;
+    }
+
+    public String getBucket() {
+        return bucket;
+    }
+
+    public void setBucket(String bucket) {
+        this.bucket = bucket;
     }
 
 
