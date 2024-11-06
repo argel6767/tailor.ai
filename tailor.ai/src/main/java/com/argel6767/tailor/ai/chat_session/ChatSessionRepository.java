a070b905de25784157a5733cb0c2acdd019e2149
argel6767 <argel6767@gmail.com>
2024-11-05 23:06:08 -0500

    add repo

---
+++ tailor.ai/src/main/java/com/argel6767/tailor/ai/chat_session/ChatSessionRepository.java
@@ -0,0 +1,6 @@
+package com.argel6767.tailor.ai.chat_session;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ChatSessionRepository extends JpaRepository<ChatSession, Long> {
+}
