f7ef5aff10cca077c5b2ca99169c8ae6b0404782
Argel Hernandez <argel6767@gmail.com>
2024-11-12 10:51:46 -0500

    add tailwind css

--- tailor-ai-frontend/src/App.jsx
+++ tailor-ai-frontend/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <>
-      <div>
+      <div className="border-red-500 space-x-10 gap-3.5">
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1 className="border-red-500 gap-3.5">Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
