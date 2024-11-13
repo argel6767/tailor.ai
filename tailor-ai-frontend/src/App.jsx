e4dce5d7b61606f633ea6d43de6695f95b0fc56a
argel6767 <argel6767@gmail.com>
2024-11-12 23:06:02 -0500

    add routes

--- tailor-ai-frontend/src/App.jsx
+++ tailor-ai-frontend/src/App.jsx
@@ -1,13 +1,20 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import {Route, Routes} from "react-router-dom";
+import LandingPage from "./pages/LandingPage.jsx";
+import AddingProfessionPage from "./pages/AddingProfessionPage.jsx";
+import ChatPage from "./pages/ChatPage.jsx";
+import AuthPage from "./pages/AuthPage.jsx";
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-      <button className="bg-red-500 border-spacing-2.5 font-bold p-6 -tracking-wide ">Button</button>
+      <Routes>
+        <Route path="/" component={LandingPage}/>
+        <Route path="/auth" component={AuthPage}/>
+        <Route path="/profession" component={AddingProfessionPage}/>
+        <Route path"/chats" component={ChatPage}/>
+      </Routes>
   )
 }
 
---
+++ tailor-ai-frontend/src/pages/LandingPage.jsx
@@ -0,0 +1,10 @@
+
+const LandingPage = () => {
+    return (
+        <div>
+            Hello This is a landing page.
+        </div>
+    )
+}
+
+export default LandingPage;
