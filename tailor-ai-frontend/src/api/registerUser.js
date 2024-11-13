674212cc29f8a234b712a31bcbe322706da956a7
argel6767 <argel6767@gmail.com>
2024-11-13 18:09:54 -0500

    add registerUser POST request

---
+++ tailor-ai-frontend/src/api/registerUser.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+const registerUser = async (authRequestValues) => {
+    try {
+        const response = await axios.post('http://localhost:8080/auth/register', authRequestValues);
+        console.log(response);
+    }
+    catch (error) {
+        console.log(error.response? error.response.data : error);
+    }
+}
