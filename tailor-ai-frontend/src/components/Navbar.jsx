1f6ad7195508fd29a89b3f31393b283cf41987b7
Argel Hernandez <argel6767@gmail.com>
2024-11-13 23:05:47 -0500

    add more styling

--- tailor-ai-frontend/src/components/Navbar.jsx
+++ tailor-ai-frontend/src/components/Navbar.jsx
@@ -2,9 +2,10 @@ import {Link} from "react-router-dom";
 
 const Navbar = () => {
     return (
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark space-x-10">
             <Link to="/" >Landing Page</Link>
             <Link to="/auth">Sign In/Up</Link>
+            <Link to="/verify">Verify Page</Link>
         </nav>
     )
 }
