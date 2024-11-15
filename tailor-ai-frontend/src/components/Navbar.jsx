5fc3ff70ff2296019f484cfcbfb34eb9eafac777
argel6767 <argel6767@gmail.com>
2024-11-14 22:05:28 -0500

    add padding to Navbar

--- tailor-ai-frontend/src/components/Navbar.jsx
+++ tailor-ai-frontend/src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import {Link} from "react-router-dom";
 
 const Navbar = () => {
     return (
-        <nav className="navbar navbar-expand-lg navbar-dark bg-base-300 gap-6 text-center">
+        <nav className="navbar navbar-expand-lg navbar-dark bg-base-300 gap-6 text-center p-2">
             <button className="btn bg-primary"> <Link to="/" >Home</Link></button>
             <button className="btn bg-primary"> <Link to="/auth">Login</Link></button>
             <Link to="/verify">Verify Page</Link> {/* for testing and view page for now */}
