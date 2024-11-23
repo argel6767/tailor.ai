fcd1c91c74f62e91ed9d065c9273bb6cbe00a478
Argel Hernandez <argel6767@gmail.com>
2024-11-23 00:37:03 -0500

    change to vercel hosting

--- tailor.ai/src/main/java/com/argel6767/tailor/ai/configs/SecurityConfiguration.java
+++ tailor.ai/src/main/java/com/argel6767/tailor/ai/configs/SecurityConfiguration.java
@@ -58,7 +58,7 @@ public class SecurityConfiguration{
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
-        corsConfiguration.setAllowedOrigins(List.of("http://localhost:5173", "https://https://tailor-ai.netlify.app"));
+        corsConfiguration.setAllowedOrigins(List.of("http://localhost:5173", "https://tailor-ai-one.vercel.app"));
         corsConfiguration.addAllowedHeader("*");
         corsConfiguration.addAllowedMethod("*");
         corsConfiguration.setAllowCredentials(true);
