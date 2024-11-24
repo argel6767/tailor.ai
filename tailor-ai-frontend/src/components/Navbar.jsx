87ebb1b40180426a3b5ab881453f60bd0819997f
Argel Hernandez <argel6767@gmail.com>
2024-11-24 02:39:40 -0500

    fix nav buttons

--- tailor-ai-frontend/src/components/Navbar.jsx
+++ tailor-ai-frontend/src/components/Navbar.jsx
@@ -3,8 +3,8 @@ import {Link} from "react-router-dom";
 const Navbar = () => {
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-base-300 gap-6 text-center p-2">
-            <button className="btn bg-primary"> <Link to="/" >Home</Link></button>
-            <button className="btn bg-primary"> <Link to="/auth">Login</Link></button>
+            <Link className="btn btn-primary" to="/" >Home</Link>
+            <Link className="btn btn-primary" to="/auth">Login</Link>
             <button className="btn bg-primary"> <a href={"https://github.com/argel6767/tailor.ai"}>Source Code</a></button>
         </nav>
     )
--- tailor-ai-frontend/src/pages/LandingPage.jsx
+++ tailor-ai-frontend/src/pages/LandingPage.jsx
@@ -1,23 +1,35 @@
 import {Link} from "react-router-dom";
+import {useState} from "react";
+import Loading from "../components/Loading.jsx";
 
 /**
  * Landing Page for Tailor.ai
  */
 const LandingPage = () => {
 
+    const [isLoading, setIsLoading] = useState(false);
+
+    const handleLoading = () => {
+        setIsLoading(!isLoading);
+    }
+
     return (
-        <div className="h-screen pt-4 px-4">
-            <div className="hero bg-primary h-2/3 rounded-lg">
-                <div className="hero-content text-center">
-                    <div className="max-w-md">
-                        <h1 className="text-5xl font-bold py-1.5">Welcome to Tailor.ai</h1>
-                        <p className="py-6 text-xl">
-                            AI-powered solutions to tailor your resume for any profession.
-                        </p>
-                        <button className="btn btn-secondary"><Link to="/auth">Get Started</Link></button>
+        <div onClick={handleLoading} className="h-screen pt-4 px-4">
+            {isLoading? <div className="flex justify-center items-center h-2/4 w-svw"><Loading loadingMessage={"Navigating to page..."}/></div> :
+                <>
+                    <div className="hero bg-primary h-2/3 rounded-lg">
+                        <div className="hero-content text-center">
+                            <div className="max-w-md">
+                                <h1 className="text-5xl font-bold py-1.5">Welcome to Tailor.ai</h1>
+                                <p className="py-6 text-xl">
+                                    AI-powered solutions to tailor your resume for any profession.
+                                </p>
+                                <Link className="btn btn-secondary" to="/auth">Get Started</Link>
+                            </div>
+                        </div>
                     </div>
-                </div>
-            </div>
+                </>}
+
             <span className="grid grid-cols-3 gap-7 w-full text-center p-9 text-background px-3">
                 <div className="bg-secondary rounded-xl hover:scale-105 transition-transform duration-300">
                     <h3 className="text-xl font-bold pt-3">Personalized Resumes</h3>
