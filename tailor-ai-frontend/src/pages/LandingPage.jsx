46cea75c8dd03c0d3415e231dee89ca3a6636b71
Argel Hernandez <argel6767@gmail.com>
2024-11-24 02:55:21 -0500

    remove loading

--- tailor-ai-frontend/src/pages/LandingPage.jsx
+++ tailor-ai-frontend/src/pages/LandingPage.jsx
@@ -7,29 +7,19 @@ import Loading from "../components/Loading.jsx";
  */
 const LandingPage = () => {
 
-    const [isLoading, setIsLoading] = useState(false);
-
-    const handleLoading = () => {
-        setIsLoading(!isLoading);
-    }
-
     return (
-        <div onClick={handleLoading} className="h-screen pt-4 px-4">
-            {isLoading? <div className="flex justify-center items-center h-2/4 w-svw"><Loading loadingMessage={"Navigating to page..."}/></div> :
-                <>
-                    <div className="hero bg-primary h-2/3 rounded-lg">
-                        <div className="hero-content text-center">
-                            <div className="max-w-md">
-                                <h1 className="text-5xl font-bold py-1.5">Welcome to Tailor.ai</h1>
-                                <p className="py-6 text-xl">
-                                    AI-powered solutions to tailor your resume for any profession.
-                                </p>
-                                <Link className="btn btn-secondary" to="/auth">Get Started</Link>
-                            </div>
-                        </div>
+        <div className="h-screen pt-4 px-4">
+            <div className="hero bg-primary h-2/3 rounded-lg">
+                <div className="hero-content text-center">
+                    <div className="max-w-md">
+                        <h1 className="text-5xl font-bold py-1.5">Welcome to Tailor.ai</h1>
+                        <p className="py-6 text-xl">
+                            AI-powered solutions to tailor your resume for any profession.
+                        </p>
+                        <Link className="btn btn-secondary" to="/auth">Get Started</Link>
                     </div>
-                </>}
-
+                </div>
+            </div>
             <span className="grid grid-cols-3 gap-7 w-full text-center p-9 text-background px-3">
                 <div className="bg-secondary rounded-xl hover:scale-105 transition-transform duration-300">
                     <h3 className="text-xl font-bold pt-3">Personalized Resumes</h3>
