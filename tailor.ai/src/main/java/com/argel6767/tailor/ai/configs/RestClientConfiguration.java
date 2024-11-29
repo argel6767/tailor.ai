c418241c0bdf420eb8f40b289bfb3917adf20e34
Argel Hernandez <argel6767@gmail.com>
2024-11-29 16:25:47 -0500

    add RestClientConfiguration

---
+++ tailor.ai/src/main/java/com/argel6767/tailor/ai/configs/RestClientConfiguration.java
@@ -0,0 +1,35 @@
+package com.argel6767.tailor.ai.configs;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestClient;
+
+/**
+ * configures the RestClient object used for the third-party LinkedIn scraper API
+ */
+@Configuration
+public class RestClientConfiguration {
+    @Value("${base.url}")
+    private String baseUrl;
+
+    @Value("${rapid.api.key}")
+    private String rapidApiKey;
+
+    @Value("${rapid.api.host}")
+    private String rapidApiHost;
+
+    /*
+     * bean to that builds the actual RestClient and to be used in the LinkedInService
+     */
+    @Bean
+    public RestClient restClient() {
+        return RestClient.builder()
+                .baseUrl(baseUrl)
+                .defaultHeaders(httpHeaders -> {
+                    httpHeaders.set("x-rapidapi-key", rapidApiKey);
+                    httpHeaders.set("x-rapidapi-host", rapidApiHost);
+                })
+                .build();
+    }
+}
