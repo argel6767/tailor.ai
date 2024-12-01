67e15afd949a8cc7a171e563cc0052a608f4d377
Argel Hernandez <argel6767@gmail.com>
2024-12-01 17:44:31 -0500

    add ChangePasswordDto request

---
+++ tailor.ai/src/main/java/com/argel6767/tailor/ai/auth/requests/ChangePasswordDto.java
@@ -0,0 +1,40 @@
+package com.argel6767.tailor.ai.auth.requests;
+
+public class ChangePasswordDto {
+
+    private String email;
+    private String oldPassword;
+    private String newPassword;
+
+    public ChangePasswordDto() {}
+
+    public ChangePasswordDto(String email, String oldPassword, String newPassword) {
+        this.email = email;
+        this.oldPassword = oldPassword;
+        this.newPassword = newPassword;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getOldPassword() {
+        return oldPassword;
+    }
+
+    public void setOldPassword(String oldPassword) {
+        this.oldPassword = oldPassword;
+    }
+
+    public String getNewPassword() {
+        return newPassword;
+    }
+
+    public void setNewPassword(String newPassword) {
+        this.newPassword = newPassword;
+    }
+}
