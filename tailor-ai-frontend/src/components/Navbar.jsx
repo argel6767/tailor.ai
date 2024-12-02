0a7ba73aaeaf5eca4da91c390048f1ce11ca09e4
Argel Hernandez <argel6767@gmail.com>
2024-12-01 22:19:11 -0500

    add UserAvatar

--- tailor-ai-frontend/src/components/Navbar.jsx
+++ tailor-ai-frontend/src/components/Navbar.jsx
@@ -1,11 +1,14 @@
 import {Link} from "react-router-dom";
+import {UserAvatar} from "./UserAvatar.jsx";
 
 const Navbar = () => {
     return (
-        <nav className="navbar navbar-expand-lg navbar-dark bg-base-300 gap-6 text-center p-2">
-            <Link className="btn btn-primary" to="/" >Home</Link>
-            <Link className="btn btn-primary" to="/auth">Login</Link>
+        <nav className="navbar  bg-base-300 gap-6 text-center p-2 px-4">
+            <Link className="btn btn-primary flex-1 max-w-24" to="/" >Home</Link>
+            <Link className="btn btn-primary flex-1 max-w-24" to="/auth">Login</Link>
             <button className="btn bg-primary"> <a href={"https://github.com/argel6767/tailor.ai"}>Source Code</a></button>
+            <div className="flex-1"></div>
+            <UserAvatar/>
         </nav>
     )
 }
