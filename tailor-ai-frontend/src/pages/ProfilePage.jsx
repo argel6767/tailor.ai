b4f824d693ce6f6b5ecbb2f3b05809fc9932c12b
argel6767 <argel6767@gmail.com>
2024-12-02 15:24:10 -0500

    add canceling button

--- tailor-ai-frontend/src/pages/ProfilePage.jsx
+++ tailor-ai-frontend/src/pages/ProfilePage.jsx
@@ -13,19 +13,31 @@ export const ProfilePage = () => {
     }
 
     return (
-        <main className="flex flex-col items-center justify-center w-full pt-2">
-            <div className="flex-1 bg-accent p-4">
-                <h1>User Settings</h1>
+        <main className="flex flex-col items-center justify-center w-full">
+            <div className="flex-1 bg-accent p-4 w-1/2">
+                <h1 className="text-4xl pb-2">User Settings</h1>
                 <hr/>
-                <h2>Account Details</h2>
-                <span className="flex flex-col items-center justify-center gap-2">
+                <h2 className="text-xl pt-4 pb-2">Account Details</h2>
+                <span className="flex flex-col max-w-60  gap-3  p-2">
                     <p>Email: {localStorage.getItem("email")}</p>
-                    <p>Password: <input type="password" placeholder="*********" className="input input-bordered input-sm" disabled={!isChangingPassword}/></p>
+                    <p>Password: <input type="password" placeholder={isChangingPassword? "Enter password" : "********"
+                    } className="input input-sm w-7/12" disabled={!isChangingPassword}/></p>
                     {isChangingPassword ?
-                        <input type="password" placeholder="Enter new password" className="input input-bordered input-sm"/> :
-                        <button className="btn btn-sm" onClick={handleIsChangingPassword}>Change Password</button>}
-                </span>
+                        <div className="flex flex-col w-10/12 gap-3">
+                            <input type="password" placeholder="Enter new password"
+                                   className="input input-sm"/>
+                            <div className="flex justify-center items-center gap-3">
+                                <button className="btn btn-primary flex-1" onClick={handleIsChangingPassword}>Cancel</button>
+                                <button className="btn btn-primary w-6/12" onClick={handleIsChangingPassword}>Submit</button>
+                            </div>
 
+                        </div>
+                        :
+                        <div className="flex justify-center items-center">
+                            <button className="btn btn-primary w-6/12" onClick={handleIsChangingPassword}>Change Password</button>
+                        </div>
+                    }
+                </span>
             </div>
         </main>
     )
---
+++ tailor.ai/src/main/java/com/argel6767/tailor/ai/fresh_linkedin/FreshLinkedInResponse.java
@@ -0,0 +1,4 @@
+package com.argel6767.tailor.ai.fresh_linkedin;
+
+public class FreshLinkedInResponse {
+}
