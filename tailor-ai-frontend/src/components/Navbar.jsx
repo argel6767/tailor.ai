4d91bb74e6edf394bd3b6cefbc869316becf1e04
Argel Hernandez <argel6767@gmail.com>
2024-12-08 22:26:41 -0500

    remove login button

--- tailor-ai-frontend/src/components/Navbar.jsx
+++ tailor-ai-frontend/src/components/Navbar.jsx
@@ -5,7 +5,6 @@ const Navbar = ({refreshApp}) => {
     return (
         <nav className="navbar  bg-base-300 gap-6 text-center p-2 px-4">
             <Link className="btn btn-primary flex-1 max-w-24" to="/" >Home</Link>
-            <Link className="btn btn-primary flex-1 max-w-24" to="/auth">Login</Link>
             <Link className="btn btn-primary flex-1 max-w-28" to="/chats">View Chats</Link>
             <button className="btn bg-primary"> <a href={"https://github.com/argel6767/tailor.ai"}>Source Code</a></button>
             <div className="flex-1"></div>
